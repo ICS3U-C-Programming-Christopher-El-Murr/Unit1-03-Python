b6d03c584c2ca469390679a02b6259dde3facbf7
Christopher El-Murr <christopher.el-murr@stu.ocsb.ca>
2025-09-20 00:07:51 +0000

    added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Christopher El-Murr
+# Date: sept 19 2025
+# This program displays hell world with proper style
+
+
+def main():
+    print("Hello, World! with proper style!")
+    print("I am testing my function here")
+
+
+if __name__ == "__main__":
+    main()
